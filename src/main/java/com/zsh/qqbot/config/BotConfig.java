8014b45471ef952271dcb35986e3cb0dff275b3a
zsh <zsh@dashuju.com>
2023-03-01 18:40:43 +0800

    登录时候自动使用不同协议

--- src/main/java/com/zsh/qqbot/config/BotConfig.java
+++ src/main/java/com/zsh/qqbot/config/BotConfig.java
@@ -1,6 +1,7 @@
 package com.zsh.qqbot.config;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactory;
 import net.mamoe.mirai.utils.BotConfiguration;
@@ -14,22 +15,33 @@ import java.io.File;
  */
 @Configuration
 @AllArgsConstructor
+@Slf4j
 public class BotConfig {
 
     private final QqProp qqProp;
 
     /**
-     * 登记qq机器人
+     * 登录qq机器人 -- 尝试不同协议登录
      */
     @Bean
     public Bot loginBot() {
-        // 使用自定义配置
-        Bot bot = BotFactory.INSTANCE.newBot(qqProp.getUsername(), qqProp.getPassword(), new BotConfiguration() {{
-            fileBasedDeviceInfo(); // 使用 device.json 存储设备信息
-            setCacheDir(new File("build/cache")); // 修改缓存目录到build/cache目录下
-            setProtocol(MiraiProtocol.ANDROID_PAD); // 登录协议
-        }});
-        bot.login();
+        Bot bot = null;
+        BotConfiguration.MiraiProtocol[] protocols = BotConfiguration.MiraiProtocol.values();
+        for (BotConfiguration.MiraiProtocol protocol : protocols) {
+            bot = BotFactory.INSTANCE.newBot(qqProp.getUsername(), qqProp.getPassword(), new BotConfiguration() {{
+                // 不同协议使用不同设备信息
+                fileBasedDeviceInfo("device/" + protocol.name() + ".json");
+                // 修改缓存目录到build/cache目录下
+                setCacheDir(new File("build/cache"));
+                setProtocol(protocol);
+            }});
+            try {
+                bot.login();
+                break;
+            } catch (Exception e) {
+                log.info("login bot failed", e);
+            }
+        }
         return bot;
     }
 }
