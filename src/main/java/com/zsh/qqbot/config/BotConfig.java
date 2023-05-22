23dc29c627ce492a79acf144dd09ea571677d252
zsh <zsh@dashuju.com>
2023-05-22 16:04:59 +0800

    device使用固定目录

--- src/main/java/com/zsh/qqbot/config/BotConfig.java
+++ src/main/java/com/zsh/qqbot/config/BotConfig.java
@@ -20,6 +20,8 @@ public class BotConfig {
 
     private final QqProp qqProp;
 
+    private final String RESOURCE_DIR = "D:\\coderLife\\githubProject\\qqbot\\src\\main\\resources\\device\\";
+
     /**
      * 登录qq机器人 -- 尝试不同协议登录
      */
@@ -28,11 +30,17 @@ public class BotConfig {
         Bot bot = null;
         ClassLoader classLoader = getClass().getClassLoader();
         String dirName = classLoader.getResource("device/").getFile();
+        if (dirName.contains("BOOT-INF/classes!")) {
+            log.info("执行jar包");
+            // jar包, 无法修改文件
+            dirName = RESOURCE_DIR;
+        }
         BotConfiguration.MiraiProtocol[] protocols = BotConfiguration.MiraiProtocol.values();
         for (BotConfiguration.MiraiProtocol protocol : protocols) {
+            String finalDirName = dirName;
             bot = BotFactory.INSTANCE.newBot(qqProp.getUsername(), qqProp.getPassword(), new BotConfiguration() {{
                 // 不同协议使用不同设备信息
-                fileBasedDeviceInfo(dirName + protocol.name() + ".json");
+                fileBasedDeviceInfo(finalDirName + protocol.name() + ".json");
                 // 修改缓存目录到build/cache目录下
                 setCacheDir(new File("build/cache"));
                 setProtocol(protocol);
